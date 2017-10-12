b2f28d838b06d18fb3bfe1cda6a9a82ed3740626
Vontrelle Collins <trelle823@msn.com>
2017-10-11 21:07:40 -0400

    Implemented operator overloads for +,-,<< for GridPoint class and added functionto calculate hScore

--- app/gridpoint.cpp
+++ app/gridpoint.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
 #include "gridpoint.hpp"
 
-GridPoint::GridPoint(int x, int y, int g, int h) : Point(x,y), g(g), h(h) {}
+GridPoint::GridPoint() {};
+
+GridPoint::GridPoint(int x, int y) : Point(x,y) {}
 
 void GridPoint::calGScore() {
-
+	std::cout << "Needs Work" << std::endl;
 }
 
 void GridPoint::calFScore() {
-	
+	std::cout << "Needs Work" << std::endl;
+}
+
+void GridPoint::calHScore() {
+	std::cout << "Needs Work" << std::endl;
+}
+
+GridPoint GridPoint::operator+(GridPoint p) {
+	GridPoint newPoint;
+	newPoint.setX(x + p.getX());
+	newPoint.setY(y + p.getY());
+
+	return newPoint;
+}
+
+GridPoint GridPoint::operator-(GridPoint p) {
+	GridPoint newPoint;
+	newPoint.setX(x - p.getX());
+	newPoint.setY(y - p.getY());
+
+	return newPoint;
+}
+
+std::ostream& operator<<(std::ostream &output, Point &p) {
+	output << "GridPoint(" << p.getX() << "," << p.getY() << ")";
+	return output;
 }
--- include/gridpoint.hpp
+++ include/gridpoint.hpp
@@ -1,16 +1,24 @@
 #ifndef GRIDPOINT_H
 #define GRIDPOINT_H
 
+#include <iostream>
 #include "point.hpp"
 
 class GridPoint : public Point {
   public:
-  	GridPoint(int x, int y, int g, int h);
+  	GridPoint();
+  	GridPoint(int x, int y);
   	void calGScore();
   	void calFScore();
+  	void calHScore();
+  	GridPoint operator+(GridPoint p);
+  	GridPoint operator-(GridPoint p);
+  	friend std::ostream& operator<<(std::ostream &, Point &);
 
   private:
   	int fScore;
   	int gScore;
   	int hScore;
 };
+
+#endif
