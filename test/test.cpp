1b8a24cb207442fb07bf9352a5fdb0531c228ccf
Vontrelle Collins <trelle823@msn.com>
2017-10-17 18:11:18 -0400

    Fixed build issues

--- test/test.cpp
+++ test/test.cpp
@@ -2,15 +2,22 @@
 
 #include "gridpoint.hpp"
 
-Test(gridpoint, test1) {
+TEST(Point, gribpoint_check) {
     GridPoint p1(2,2);
     GridPoint p2(3,3);
     GridPoint p3(5,5);
 
-    ASSERT_GE(p1.calG(p2), 10);
-    ASSERT_GE(p1.calF(), 10);
-    ASSERT_GT(p1.calH(p2), 0);
+    GridPoint a = p1+p2;
+    GridPoint b = p3-p1;
 
-    ASSERT_EQ(p3, (p1+p2));
-    ASSERT_EQ(p2, (p3-p1));
+    ASSERT_TRUE(p3==a);
+    ASSERT_TRUE(p2==b);
+
+    p1.calH(p3);
+    p1.calG(p2);
+    p1.calF();
+
+    ASSERT_GE(p1.getG(), 10);
+    ASSERT_GT(p1.getH(), 0);
+    ASSERT_GT(p1.getF(), 0);
 }
